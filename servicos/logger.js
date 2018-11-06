9cb2babbeedc4b51be25ce4777a258d1472b0a35
Leonardo Lopes <leonardo.lopes@camisariafascynios.com.br>
2018-11-06 14:35:08 -0200

    Criando objeto do Winston que realiza o log

---
+++ servicos/logger.js
@@ -0,0 +1,13 @@
+const winston = require("winston");
+
+module.exports = winston.createLogger({
+    transports: [
+        new winston.transports.File({
+            level: "info",
+            filename: "logs/payfast.log",
+            maxsize: 100000,
+            maxFiles: 10
+        })
+    ]
+});
+
