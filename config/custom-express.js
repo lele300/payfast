ebf1a61cab46f8946ead49c97c90fd990b9b89b7
Leonardo Lopes <leonardo.lopes@camisariafascynios.com.br>
2018-11-06 15:57:51 -0200

    Adicionando o Morgan para tratar as requisições e logar com o Winston | Criando middleware no Express do Morgan

--- config/custom-express.js
+++ config/custom-express.js
@@ -2,9 +2,18 @@ const express = require("express");
 const consign = require("consign");
 const bodyParser = require("body-parser");
 const expressValidator = require("express-validator");
+const morgan = require("morgan"); //Intercepta a requisição do Express
+const logger = require("../servicos/logger"); //Utilizado para escrever logs
 
 module.exports = () => {
     const app = express();
+    app.use(morgan("common", { // .morgan(formato, options)
+        stream : { //Utilizado para não parar a execução do programa para escrever logs
+            write : message => { //Tudo que o padrão common(headers, body do request, etc...) fica disponível na variável message 
+                logger.info(message);
+            }
+        }
+    }));
     app.use(bodyParser.urlencoded({extended : true}));
     app.use(bodyParser.json());
     app.use(expressValidator());
